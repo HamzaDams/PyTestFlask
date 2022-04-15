ad85fbdd4cbf69ab1f9c63d5171218d7e7587767
hamzadams <hddesignx@gmail.com>
2022-04-15 16:09:05 +0200

    First

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, request
-import requests
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
 @app.route("/other")
 def hello_other():
-    page = request.args.get('page', default = 1, type = int)
+    page = request.args.get('page', default=1, type=int)
     return f"<p>Hello, Other!</p><p>Page : {page}<:p>"
 
 
@@ -17,9 +18,3 @@ def hello_other():
 def exp():
     value = int(request.args.get('value'))
     return f"<p>Exposant 2 de {value} : {pow(value, 2)}</p>"
-
-@app.route("/coincoin")
-def coincoin():
-    response = requests.get('https://random-d.uk/api/random')
-    img = response.json().get('url')
-    return f"<img src='{img}' />"
